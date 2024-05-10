795874c228247856048190ff9ddcfa46472b0495
Hernán Pereira <pereirahernan@outlook.com>
2024-05-09 20:33:50 -0500

    feat: :sparkles: add meeting setup component

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,25 +1,21 @@
-import { Sidebar } from '@/components/Sidebar'
-import { Navbar } from '@/components/Navbar'
-import React, { ReactNode } from 'react'
+import { Sidebar } from "@/components/Sidebar";
+import { Navbar } from "@/components/Navbar";
+import React, { ReactNode } from "react";
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
-    return (
-        <main className='relative'>
-            <Navbar />
+  return (
+    <main className="relative">
+      <Navbar />
 
-            <div className="flex">
-                <Sidebar />
+      <div className="flex">
+        <Sidebar />
 
-                <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
-                    <div className="w-full">
-                        {children}
-                    </div>
-                </section>
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
 
-            </div>
-
-        </main>
-    )
-}
-
-export default HomeLayout
+export default HomeLayout;
