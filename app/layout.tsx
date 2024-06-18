e9ac19fe8e4b509a160f6a3f856c492c7dd2a13b
Hernán Pereira <pereirahernan@outlook.com>
2024-06-18 11:39:19 -0500

    feat: add meeting room

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
@@ -17,19 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
-      layout: {
-        logoImageUrl: '/icons/yoom-logo.svg',
-        socialButtonsVariant: 'iconButton'
-      },
-      variables:{
-        colorText: '#fff',
-        colorPrimary: '#0E78F9',
-        colorBackground: '#1C1F2E',
-        colorInputBackground: '#252A41',
-        colorInputText: '#fff'
-      }
-    }}>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: "/icons/yoom-logo.svg",
+          socialButtonsVariant: "iconButton",
+        },
+        variables: {
+          colorText: "#fff",
+          colorPrimary: "#0E78F9",
+          colorBackground: "#1C1F2E",
+          colorInputBackground: "#252A41",
+          colorInputText: "#fff",
+        },
+      }}
+    >
       <html lang="en">
         <body className={`${inter.className} bg-dark-2`}>
           {children}
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutList } from "lucide-react";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
-  return (
-    <div>MeetingRoom</div>
-  )
-}
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
 
-export default MeetingRoom
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
+
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div
+          className={cn("h-[calc(100hv-86px)] hidden ml-2", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
+        <DropdownMenu>
+          <div className="flex items-center">
+          <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d]">
+            <LayoutList size={23} className="text-white"/>
+          </DropdownMenuTrigger>
+
+          </div>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </section>
+  );
+};
+
+export default MeetingRoom;
